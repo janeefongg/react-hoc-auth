8d05e154268886aeecc1b2cd5e2edaca981c8b1b
Jane Fong <janeefongg@gmail.com>
2016-06-20 17:27:47 -0700

    [setup] react router

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Recipe
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    </div>
+  )
+}
